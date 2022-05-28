887f095b93c5f48049cbd35c129f4d12a76b411c
Julien Verneaut <jverneaut@gmail.com>
2022-05-28 16:18:47 +0200

    Add example folder

--- packages/sandbox/bin/cli.js
+++ packages/sandbox/bin/cli.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 const cac = require('cac');
 
+const build = require('../src/build');
+const dev = require('../src/dev');
+
 const cli = cac('@jverneaut/sandbox');
-cli.command('build', 'Build assets');
+
+cli.command('build', 'Build assets').action(() => {
+  build();
+});
+
+cli.command('dev', 'Launch dev server').action(() => {
+  dev();
+});
 
 cli.help();
 cli.parse();
-cli.outputHelp();
+
+if (!cli.matchedCommandName && Object.keys(cli.options).length <= 1) {
+  cli.outputHelp();
+}
