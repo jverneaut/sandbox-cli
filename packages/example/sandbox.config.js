18b2dbc2f05a4aa0c296c519972a4178e54ff80a
Julien Verneaut <jverneaut@gmail.com>
2022-08-24 01:23:56 +0200

    Allow webpack config customization

---
+++ packages/example/sandbox.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  },
+};
