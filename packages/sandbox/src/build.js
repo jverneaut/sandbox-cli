b97e9f0e0134822723abb1859a5580e91dbd2bcc
Julien Verneaut <jverneaut@gmail.com>
2022-08-24 04:48:26 +0200

    Handle images

--- packages/sandbox/src/build.js
+++ packages/sandbox/src/build.js
@@ -1,8 +1,22 @@
 const webpack = require('webpack');
-const config = require('./webpack.config');
+const sandboxConfig = require('./webpack.config');
+const { merge } = require('webpack-merge');
+const glob = require('glob');
+const path = require('path');
 
 module.exports = () => {
-  webpack({ ...config, mode: 'production' }, (err, stats) => {
+  const clientConfigPath = glob.sync(
+    path.join(process.cwd(), 'sandbox.config.js')
+  );
+
+  const clientConfig = clientConfigPath.length
+    ? require(clientConfigPath[0])
+    : {};
+
+  const devConfig = { mode: 'production' };
+  const config = merge(sandboxConfig, devConfig, clientConfig.webpack || {});
+
+  webpack(config, (err, stats) => {
     stats.toString({
       chunks: false,
       colors: true,
